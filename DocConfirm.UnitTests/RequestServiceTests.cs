482aea4da3f62c3678cb241ce58aa25d46b2887e
Nikolay Merkushov <nmerkushov@mail.ru>
2018-04-23 11:05:34 +0300

    Update RequsetServiceTests

--- DocConfirm.UnitTests/RequestServiceTests.cs
+++ DocConfirm.UnitTests/RequestServiceTests.cs
@@ -1,10 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Xunit;
 using NSubstitute;
-using LinqToDB;
 using DocConfirm.Models;
 using DocConfirm.Services;
 using DocConfirm.Linq2DBContext;
@@ -16,19 +13,35 @@ namespace DocConfirm.UnitTests
 		private static List<Request> fakeRequests = new List<Request>();
 
 		[Fact]
-		public async void Test1()
+		public async void SelectRequestTest()
 		{
 			fakeRequests = TestRequestList();
-			var fakeDb = FakeDbContext();		
+			var fakeDb = FakeDbContext();
 			RequestService requestService = new RequestService(fakeDb);
 
+			RequestComparer requestComparer = new RequestComparer();
+
 			var rlist = await requestService.GetRequests();
-			var r3 = await requestService.GetRequest("RID3");		
+			Assert.Equal(TestRequestList(), rlist, requestComparer);
+
+			var r3 = await requestService.GetRequest("RID3");
+			Assert.Equal(r3, new Request
+			{
+				RequestID = 3,
+				Req_ID = "RID3",
+				NotaryID = "NT3",
+				DocDate = new DateTime(2018, 4, 27),
+				DocNum = "DN3",
+				Source = "Src3",
+				ExternalID = "ExID3",
+				ResolutionValue = true
+			},
+						requestComparer);
+
 		}
 
 		private IDocConfirmDB FakeDbContext()
 		{
-
 			var FakeDB = Substitute.For<IDocConfirmDB>();
 
 			FakeDB.Requests.Returns(
@@ -48,6 +61,29 @@ namespace DocConfirm.UnitTests
 					new Request{ RequestID=4,Req_ID="RID4",NotaryID="NT4",DocDate=new DateTime(2018,4,28),DocNum="DN4",Source="Src4",ExternalID="ExID4",ResolutionValue=false},
 				};
 			return testRequests;
+		}
+	}
+
+
+	public class RequestComparer : IEqualityComparer<Request>
+	{
+		public bool Equals(Request x, Request y)
+		{
+			return (x.Req_ID == y.Req_ID &&
+				x.NotaryID == y.NotaryID &&
+				x.DocNum == y.DocNum &&
+				x.DocDate == y.DocDate &&
+				x.DocBody == y.DocBody &&
+				x.Source == y.Source &&
+				x.ResolutionValue == y.ResolutionValue &&
+				x.ResolutionText == y.ResolutionText &&
+				x.ResolutionTextSign == y.ResolutionTextSign &&
+				x.ResolutionDateTime == y.ResolutionDateTime);
+		}
+
+		public int GetHashCode(Request rq)
+		{
+			return rq.GetHashCode();
 		}
 	}
 }
